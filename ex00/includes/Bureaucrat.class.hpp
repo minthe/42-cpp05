e2a0891af8e779aaf1db495eb3a1fc3373652bf8
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2023-05-06 23:06:26 +0200

    a.

--- ex00/includes/Bureaucrat.class.hpp
+++ ex00/includes/Bureaucrat.class.hpp
@@ -6,13 +6,17 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 15:34:08 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/04/13 15:53:21 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2023/05/06 23:04:44 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
 
 class Bureaucrat
 {
     
     private:
 
+        std::string _name const;
+        int         _grade;
 };
